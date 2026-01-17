35c15e29b6dd725ad5ce287f15e7a3377d7b9e7b
revan <revanthnagidi1@gmail.com>
2026-01-17 15:31:06 +0530

    fixed errors in code

--- src/test/java/com/example/HelloDevOpsTest.java
+++ src/test/java/com/example/HelloDevOpsTest.java
@@ -5,10 +5,10 @@ System.out.println("Testing HelloDevOps class...");
 String message = HelloDevOps.getMessage();
 System.out.println("Message: " + message);
 if (message.equals("Hello from DevOps Training")) {
-System.out.println("í°€ Test passed!");
+System.out.println("Test passed!");
 System.exit(0);
 } else {
-System.out.println("í°€ Test failed!");
+System.out.println("Test failed!");
 System.exit(1);
 }
 }
